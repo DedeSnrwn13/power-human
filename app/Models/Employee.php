0a4fd97a4a5b05c2188f9ae426424c3b12e720f8
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-03-19 22:19:29 +0700

    feat: adding relation

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'age',
+        'gender',
+        'phone',
+        'photo',
+        'team_id',
+        'role_id',
+        'is_verified',
+        'verfied_at'
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
