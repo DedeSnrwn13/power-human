20ee8f359569e5e0d2c7f5cb050bb115b6ac4d08
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-05-20 10:52:26 +0700

    feat: api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/company', [CompanyController::class, 'all']);
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('logout', [UserController::class, 'logout']);
+    Route::get('user', [UserController::class, 'fetch']);
 });
